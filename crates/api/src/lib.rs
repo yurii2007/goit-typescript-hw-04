2dc0bd14ada3f9d2e70c33c847d961acda3fde4f
yurii2007 <yurash862@gmail.com>
2026-04-22 20:19:53 +0300

    fix: broken sqlx dependency

--- crates/api/src/lib.rs
+++ crates/api/src/lib.rs
@@ -6,6 +6,23 @@ mod config;
 pub use config::ApiConfig;
 
 pub async fn run(config: ApiConfig) -> Result<(), std::io::Error> {
+    // let redis_store = RedisSessionStore::new(&config.redis_url)
+    //     .await
+    //     .expect("Could not establish connection with Redis");
+
+    // let secret_key = Key::from(config.session_secret.as_bytes());
+    //
+    // let google_provider = Arc::new(GoogleProvider::new(
+    //     &config.google_client_id,
+    //     &config.google_client_secret,
+    //     &config.google_redirect_uri,
+    // ));
+    //
+    // let auth_state = web::Data::new(AuthState {
+    //     provider: google_provider,
+    //     db: db.clone(),
+    // });
+
     HttpServer::new(move || {
         let cors = Cors::default()
             .allowed_origin(&config.client_internal_hostname)
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use api::ApiConfig;
 use dotenv::dotenv;
-use sqlx::postgres::PgPoolOptions;
+use infrastructure::bootstrap_db;
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
@@ -8,28 +8,8 @@ async fn main() -> Result<(), std::io::Error> {
 
     let api_config = ApiConfig::from_env().expect("Failed to load config");
 
-    let db = PgPoolOptions::new()
-        .max_connections(5)
-        .connect(&api_config.database_url)
-        .await
-        .expect("Could not establish database connection");
+    let db = bootstrap_db(&api_config.database_url).await;
 
     println!("Connected to the postgres");
-    // let redis_store = RedisSessionStore::new(&config.redis_url)
-    //     .await
-    //     .expect("Could not establish connection with Redis");
-
-    // let secret_key = Key::from(config.session_secret.as_bytes());
-    //
-    // let google_provider = Arc::new(GoogleProvider::new(
-    //     &config.google_client_id,
-    //     &config.google_client_secret,
-    //     &config.google_redirect_uri,
-    // ));
-    //
-    // let auth_state = web::Data::new(AuthState {
-    //     provider: google_provider,
-    //     db: db.clone(),
-    // });
     api::run(api_config).await
 }
